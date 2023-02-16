10f4893dec92251f60c6f339d7ef1adb39fad093
Arthur Araujo <technoasti@gmail.com>
2023-02-16 18:25:30 -0300

    Req 05: Criando classes que herdam Archetype

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,27 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+export default class Warrior extends Archetype {
+  private _energy: EnergyType;
+  private static _instance = 0;
+
+  constructor(name: string) {
+    super(name);
+
+    this._energy = 'stamina';
+
+    Warrior.instanceOf();
+  }
+
+  get energyType(): EnergyType {
+    return this._energy;
+  }
+
+  static instanceOf(): void {
+    this._instance += 1;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this._instance;
+  }
+}
